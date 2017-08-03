9f046b8b86d39a03d3411cd40d3a3dadafd25552
Keith <keith.groves@jointheleague.org>
2017-08-03 10:21:46 -0700

    added bootstrap

--- src/main/java/com/bytebistro/weatherservice/Application.java
+++ src/main/java/com/bytebistro/weatherservice/Application.java
@@ -2,12 +2,18 @@ package com.bytebistro.weatherservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.thymeleaf.spring4.processor.attr.SpringActionAttrProcessor;
 
 @SpringBootApplication
-public class Application {
-
+public class Application extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+   
 }
--- src/main/java/com/bytebistro/weatherservice/WebController.java
+++ src/main/java/com/bytebistro/weatherservice/WebController.java
@@ -1,6 +1,7 @@
 package com.bytebistro.weatherservice;
 
 import javax.validation.Valid;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -9,25 +10,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
-public class WebController extends WebMvcConfigurerAdapter {
-
-    @Override
+public class WebController  extends WebMvcConfigurerAdapter{
+@Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/signup-result").setViewName("signup-result");
+        registry.addViewController("/hello").setViewName("hello");
     }
 
-    @GetMapping("/")
+    @GetMapping("/signup")
     public String showForm(SignupForm signupForm) {
         return "signup";
     }
 
-    @PostMapping("/")
+    @PostMapping("/signup")
     public String checkSignupInfo(@Valid SignupForm signupForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors())
-            return "form";
+            return "results";
         else
-            return "redirect:/signup-result";
+            return "results";
+    }
+      @RequestMapping("/results")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="Person") String name) {
+        model.addAttribute("name", name);
+        return "results";
     }
 }
